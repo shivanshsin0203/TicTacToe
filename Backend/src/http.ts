205890c4d67c62be892c9c05c1e0cbdb753b0d88
shivanshsin0203 <singhshivansh12may@gmail.com>
2026-02-20 18:15:12 +0530

    DB setup

--- Backend/src/http.ts
+++ Backend/src/http.ts
@@ -8,4 +8,9 @@ export function initHttp(app: Express) {
     app.get("/", (req, res) => {
         res.json({message: "System is up"});
     });
+    app.post("/register", (req, res) => {
+        const {name, email, password} = req.body;
+        console.log(name, email, password);
+        res.json({message: "User registered successfully"});
+    })
 }
